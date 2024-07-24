3408f51f7d5de7e8f71af303f0c6289295703c2e
Diego Rivas <darivas@uce.edu.ec>
2024-07-24 12:18:16 -0500

    Cambio en el inicio y redireecion a las peliculas, rankin, y proximos con el nickname en la pagina

---
+++ app/src/main/java/com/rivas/diego/proyectorivas/ui/fragments/login/ProfileSettingsFragment.kt
@@ -0,0 +1,4 @@
+package com.rivas.diego.proyectorivas.ui.viewmodels.login
+
+class ProfileSettingsFragment {
+}
---
+++ app/src/main/java/com/rivas/diego/proyectorivas/ui/fragments/main/MoviesFragment.kt
@@ -0,0 +1,13 @@
+package com.rivas.diego.proyectorivas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class moviesFragment : Fragment() {
+
+}
